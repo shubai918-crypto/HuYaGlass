35100be933442cb7eb8a33d0226a64a927bcaa7d
shubai918-crypto <shubai918@gmail.com>
2026-08-17 13:11:04 +0800

    Update live_core.dart

--- live_core/lib/live_core.dart
+++ live_core/lib/live_core.dart
@@ -1,10 +1,19 @@
 library live_core;
 
+// Tars 协议
+export 'src/tars/tars_reader.dart';
+export 'src/tars/tars_writer.dart';
+export 'src/tars/tars_struct.dart';
+export 'src/tars/huya_structs.dart';
+
+// 虎牙
 export 'src/huya/huya_api.dart';
 export 'src/huya/huya_stream.dart';
 export 'src/huya/huya_danmaku.dart';
-export 'src/huya/tars/tars_reader.dart';
-export 'src/huya/tars/tars_writer.dart';
+export 'src/huya/huya_login.dart';
+
+// 模型
 export 'src/model/live_room.dart';
 export 'src/model/danmaku_message.dart';
 export 'src/model/stream_quality.dart';
+export 'src/model/streamer_info.dart';
