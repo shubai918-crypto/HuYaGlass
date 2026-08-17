6448c90b5138ae2cc43aac88cb6be955a4d1e844
shubai918-crypto <shubai918@gmail.com>
2026-08-17 13:20:27 +0800

    Update live_room.dart

--- live_core/lib/src/model/live_room.dart
+++ live_core/lib/src/model/live_room.dart
@@ -7,6 +7,8 @@ class LiveRoom {
   final bool isLive;
   final String coverUrl;
   final String platform;
+  final String categoryName;
+  final int presenterUid;
 
   LiveRoom({
     required this.roomId,
@@ -17,17 +19,34 @@ class LiveRoom {
     required this.isLive,
     required this.coverUrl,
     this.platform = 'huya',
+    this.categoryName = '',
+    this.presenterUid = 0,
   });
 
   factory LiveRoom.fromJson(Map<String, dynamic> json) {
     return LiveRoom(
-      roomId: json['roomId'] ?? '',
+      roomId: json['roomId']?.toString() ?? '',
       title: json['title'] ?? '',
       streamerName: json['streamerName'] ?? '',
       streamerAvatar: json['streamerAvatar'] ?? '',
       fansCount: json['fansCount'] ?? 0,
       isLive: json['isLive'] ?? false,
       coverUrl: json['coverUrl'] ?? '',
+      categoryName: json['categoryName'] ?? '',
+      presenterUid: json['presenterUid'] ?? 0,
     );
   }
+
+  Map<String, dynamic> toJson() => {
+    'roomId': roomId,
+    'title': title,
+    'streamerName': streamerName,
+    'streamerAvatar': streamerAvatar,
+    'fansCount': fansCount,
+    'isLive': isLive,
+    'coverUrl': coverUrl,
+    'platform': platform,
+    'categoryName': categoryName,
+    'presenterUid': presenterUid,
+  };
 }
