61269b7374fcda60665611b8b669ab5de77b92f7
shubai918-crypto <shubai918@gmail.com>
2026-08-17 12:40:14 +0800

    Create live_core.dart

---
+++ live_core/lib/live_core.dart
@@ -0,0 +1,10 @@
+library live_core;
+
+export 'src/huya/huya_api.dart';
+export 'src/huya/huya_stream.dart';
+export 'src/huya/huya_danmaku.dart';
+export 'src/huya/tars/tars_reader.dart';
+export 'src/huya/tars/tars_writer.dart';
+export 'src/model/live_room.dart';
+export 'src/model/danmaku_message.dart';
+export 'src/model/stream_quality.dart';
