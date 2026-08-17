8e95b5d64bcc55c5ebdaa930dcd7036ba1c1a433
shubai918-crypto <shubai918@gmail.com>
2026-08-17 12:48:45 +0800

    Create main.dart

---
+++ live_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'app/routes.dart';
+import 'app/theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
+  await Hive.openBox('follow');
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'HuyaLive',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      getPages: AppRoutes.pages,
+      initialRoute: AppRoutes.home,
+    );
+  }
+}
