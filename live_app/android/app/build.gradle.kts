d143b7f48b8f89c4ee0b0db68f78c857e2f51ac0
shubai918-crypto <shubai918@gmail.com>
2026-08-22 09:47:42 +0800

    Update build.gradle.kts

--- live_app/android/app/build.gradle.kts
+++ live_app/android/app/build.gradle.kts
@@ -25,7 +25,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    
+    kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
 
     defaultConfig {
         applicationId = "com.bs.live_app"
