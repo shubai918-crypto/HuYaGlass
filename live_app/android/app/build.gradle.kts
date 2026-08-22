4ee342c808b4b7ab27b8278618f5d8e40e1c5385
shubai918-crypto <shubai918@gmail.com>
2026-08-22 09:58:48 +0800

    Update build.gradle.kts

--- live_app/android/app/build.gradle.kts
+++ live_app/android/app/build.gradle.kts
@@ -25,11 +25,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-    }
-}
+    
 
     defaultConfig {
         applicationId = "com.bs.live_app"
@@ -60,6 +56,12 @@ android {
                 "proguard-rules.pro"
             )
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
